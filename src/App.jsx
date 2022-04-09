158672dc9570254edd387ca747d3593144954a24
Alexandre Uglar <acuglar@gmail.com>
2022-04-09 18:42:22 -0300

    Fetch api url

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+import Chars from './components/Chars';
+import './App.css';
+
+class App extends Component {
+	state = {
+		characters: [],
+	};
+
+	componentDidMount() {
+		fetch('https://rickandmortyapi.com/api/character')
+			.then(response => response.json())
+			.then(response => this.setState({ characters: response.results }));
+
+		console.log(this.state.characters); // []
+	}
+
+	render() {
+		console.log(this.state.characters); // (20) [{...}]
+
+		return (
+			<div className='App'>
+				dofsdjfo
+				<Chars />
+			</div>
+		);
+	}
+}
+
+export default App;
